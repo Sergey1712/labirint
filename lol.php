5e7c372a4e3afc560058a7a5f203caf58814de62
Сергей <s89525991117@gmail.com>
2019-01-15 22:22:00 +0300

    ничего

--- lol.php
+++ lol.php
@@ -1,6 +1,16 @@
 <?php
-setcookie('кука', 'готова');
-if ($_COOKIE['кука'] != '') {
-    echo $_COOKIE['кука'];
-}
+$show_flag = false;
 ?>
+ 
+<html>
+<body>
+<?php if ($show_flag) : ?>
+<div id="help">
+<div>
+<p>asdfgh</p>
+</div>
+<a>http://cyberforum.ru</a>
+</div>
+<?php endif; ?>
+</body>
+</html>
